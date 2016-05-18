ae631180312ccea56a1c865cf8401fb7fde5a810
Dave Gravelle <dgravelle54@gmail.com>
2016-05-17 21:35:23 -0700

    api routing initialized

---
+++ routes/api.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const router = require('express').Router();
+const knex = require('../db/knex');
+
+router.get('/todolist', (req, res, next) => {
+  knex('to_do_list').select('*').then((data) => {
+    console.log(data);
+    res.json(data);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.json(err);
+  })
+})
+
+module.exports = router;
